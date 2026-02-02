9083990cf8477ecdbd93812817d58678ddac09bb
JuraSciix <jurasciix@gmail.com>
2026-02-03 01:09:36 +0300

    Добавляет `MainActivity.navController`

--- app/src/main/java/org/jurasciix/quicknotes/MainActivity.kt
+++ app/src/main/java/org/jurasciix/quicknotes/MainActivity.kt
@@ -2,15 +2,26 @@ package org.jurasciix.quicknotes
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
-    private val viewModel: MainViewModel by viewModels()
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
